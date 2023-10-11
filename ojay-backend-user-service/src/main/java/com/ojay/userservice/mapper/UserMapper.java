ac8dd3d09ae9d8f1b097b6f39450998a5657b3c2
dexter <850717623@qq.com>
2023-10-11 15:15:18 +0800

    添加user-service模块

---
+++ ojay-backend-user-service/src/main/java/com/ojay/userservice/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ojay.userservice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ojay.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
